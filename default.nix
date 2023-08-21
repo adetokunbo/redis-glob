7d82d6bcccc7a1e603470c078f12035b08ddaf4b
Tim Emiola <adetokunbo@users.noreply.github.com>
2023-08-21 13:49:13 +0900

    Use a new haskell.nix environment

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 let
-  # Pin haskell-nix to a recent (as of 2022/11/23) commit
-  h8x-pin = "https://github.com/input-output-hk/haskell.nix/archive/983c6d74b9d2de9bb195392974c4b861a78160e2.tar.gz";
+  # Pin haskell-nix to a recent (as of 2023/07/20) commit
+  h8x-pin = "https://github.com/input-output-hk/haskell.nix/archive/3ae883bb7864ba85c3aab30f8f488b9a7d84afc8.tar.gz";
   h8x-src = builtins.fetchTarball h8x-pin;
   h8x = import h8x-src {};
 
@@ -11,7 +11,7 @@ let
     # hence you will be more likely to get cache hits when using these. But you
     # can also just use your own, e.g. '<nixpkgs>'.
 
-    h8x.sources.nixpkgs-2205
+    h8x.sources.nixpkgs-2305
 
     # These arguments passed to nixpkgs, include some patches and also the
     # haskell.nix functionality itself as an overlay.
@@ -23,12 +23,11 @@ in pkgs.haskell-nix.cabalProject {
   src = pkgs.haskell-nix.haskellLib.cleanGit {
     name = "redis-glob";
     src = ./.;
-
   };
 
   # Specify the GHC version to use.
-  compiler-nix-name = "ghc8107";
+  compiler-nix-name = "ghc928";
 
   # Specify the hackage index state
-  index-state = "2022-11-23T00:00:00Z";
+  index-state = "2023-07-20T00:00:00Z";
 }
